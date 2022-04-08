019a107da293792f8a13597a54562a8c3a63b0ac
Nirupamraj Kondamudi <nirupamraj.k@CaprusIT.local>
2022-04-08 12:24:16 +0530

    intial commit

---
+++ src/main/java/com/travels/rajbus/RajbusApplication.java
@@ -0,0 +1,13 @@
+package com.travels.rajbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RajbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RajbusApplication.class, args);
+	}
+
+}
